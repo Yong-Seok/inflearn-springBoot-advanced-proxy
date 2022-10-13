5359add86d64296482b8db1e1801cb6d5f7bbeaf
yongs21 <yongs21@douzone.com>
2022-10-13 13:37:26 +0900

    스프링이 지원하는 프록시 팩토리 예제 적용1

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +13,10 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
 // V3는 @SpringBootApplication 어노테이션 안의 @ComponentScan에 의해 스캔되므로 별도 설정파일 Import 없어도 됨
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")	// 주의. hello.proxy.config 패키지는 @Import를 사용해 적용
 public class ProxyApplication {
 	// SpringBoot run!!
